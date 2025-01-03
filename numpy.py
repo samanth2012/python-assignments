79acce7b82001adae88cbf2b469a607c4eb6e6c3
Samanth <114215621+samanth2012@users.noreply.github.com>
2025-01-03 10:26:13 +0530

    random permutations

--- numpy.py
+++ numpy.py
@@ -182,3 +182,6 @@ np.cosh() -->  finding the hyperbolic cos value
 # random distribution
 x = random.choice([3, 5, 7, 9], p=[0.1, 0.3, 0.6, 0.0], size=(3, 5)) --> 2d array
 
+#random permutation
+x=random.permutation([1,2,3)--> [3,2,1]
+
