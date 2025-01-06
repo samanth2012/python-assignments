99b66b7b0776ae7779424681908e0cfd752d5f69
Samanth <114215621+samanth2012@users.noreply.github.com>
2025-01-06 12:02:40 +0530

    cleaning empty cells

--- pandasassignment.py
+++ pandasassignment.py
@@ -12,3 +12,8 @@ df=pd.head(10) #print first 10 rows
 print(df)
 df1=pd.tail(10) #print last 10 rows
 print(df)
+
+#cleaning empty cells
+print(df.dropna) #returns new datafarame by removing empty cells
+df.fillna(130) # replacing empty cells with the value "130"
+dropna(inplace=True) #removes empty cells returns original DataFrame instead of returning a new one.
