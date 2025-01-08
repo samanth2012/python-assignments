0045d60f5c4061ed8130ca204644f3e0e16ba6a3
Samanth <114215621+samanth2012@users.noreply.github.com>
2025-01-08 09:07:50 +0530

    pandas plotting

--- pandasassignment.py
+++ pandasassignment.py
@@ -17,9 +17,17 @@ print(df)
 print(df.dropna) #returns new datafarame by removing empty cells
 df.fillna(130) # replacing empty cells with the value "130"
 dropna(inplace=True) #removes empty cells returns original DataFrame instead of returning a new one.
+
+
 #cleaning wrong format
 to_datetime() #method for converting a column into date formats?
+
 #cleaning duplicates
 df.drop_duplicates() --> removes duplicates
 #correlation
 df.corr()-->prints correlation matrix
+
+#Plotting
+df.plot() -->plotting
+df.plot(kind='scatter', x = 'Duration', y = 'Calories') --> scatter
+df['Duration'].plot(kind='hist') --> --> histogram
